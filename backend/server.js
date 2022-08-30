c6aeedcd3241e77748b2e919c158564271fb87e4
pcctonyt <104700781+pcctonyt@users.noreply.github.com>
2022-08-30 12:59:06 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,3 @@
+// Brad Traversy tutorial series for MERN full-stack goals app
+// first video, get the REST API, second video, authentication, then a dashboard for goals, front end he's wanting to use Redux and Redux toolkit, smaller version of React Ticket application full 2022 course
+console.log("Hello World");
